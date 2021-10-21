be300fa616ca541dfb651be78fa0258534d86b1d
Danilo dos Santos Alves Santana <danilosantana900@gmail.com>
2021-10-20 22:11:11 -0300

    Adicionado Console Application no projeto com injeção de dependência

---
+++ FizzBuzzTdd.App/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FizzBuzzTdd.Domain;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FizzBuzzTdd.App
+{
+    public class Program
+    {
+        private static readonly CancellationTokenSource tokenSource = new();
+        static Task Main(string[] args)
+        {
+            var hostBuilder = Host.CreateDefaultBuilder(args);
+            hostBuilder.ConfigureServices((_, services) =>
+            {                
+                services.AddTransient<FizzBuzz>();
+                services.AddHostedService<Startup>();
+                services.AddSingleton(tokenSource);
+            });
+
+            using var host = hostBuilder.Build();
+
+            return host.RunAsync(tokenSource.Token);
+        }
+    }
+}
---
+++ FizzBuzzTdd.App/Startup.cs
@@ -0,0 +1,37 @@
+﻿using FizzBuzzTdd.Domain;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FizzBuzzTdd.App
+{
+    public class Startup : IHostedService
+    {
+        private readonly FizzBuzz _fb;
+        private readonly CancellationTokenSource _cancellationTokenSource;
+
+        public Startup(FizzBuzz fb, CancellationTokenSource cancellationTokenSource)
+        {
+            _fb = fb;
+            _cancellationTokenSource = cancellationTokenSource;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine($"Resultado: {_fb.GetValue(i)}");
+            }            
+
+            _cancellationTokenSource.Cancel();
+            return;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Parando a aplicação...");
+            return;
+        }
+    }
+}
