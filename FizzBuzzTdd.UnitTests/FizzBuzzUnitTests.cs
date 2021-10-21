faae7cfac4a117efc7be4eb35f6205d835176553
Danilo dos Santos Alves Santana <danilosantana900@gmail.com>
2021-10-20 21:41:31 -0300

    Refatorado Teste Unitários para usar [Theory] ao invés do [Fact] possibilitando várias chamadas com valores diferentes.

--- FizzBuzzTdd.UnitTests/FizzBuzzUnitTests.cs
+++ FizzBuzzTdd.UnitTests/FizzBuzzUnitTests.cs
@@ -12,66 +12,69 @@ namespace FizzBuzzTdd.UnitTests
             sut = new FizzBuzz();
 	    }
 
-        [Fact]
-        public void FizzBuzz_When1_Returns1()
-        {
-            // Arrange            
-            var expected = 1;
-
-            // Act
-            var actual = sut.GetValue(expected);
-
-            // Assert
-            Assert.Equal("1", actual);
-        }
-
-        [Fact]
-        public void FizzBuzz_When2_Returns2()
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(11)]
+        [InlineData(13)]
+        [InlineData(14)]
+        public void FizzBuzz_WhenDefault_ReturnsInput(int input)
         {
             //Arrange
-            var expected = 2;
+            var expected = input.ToString();
 
-            // Act
-            var actual = sut.GetValue(expected);
+            //Act
+            var actual = sut.GetValue(input);
 
-            // Assert
-            Assert.Equal("2", actual);
+            //Assert
+            Assert.Equal(expected, actual);
         }
 
-        [Fact]
-        public void FizzBuzz_When3_ReturnFizz()
+        [Theory]
+        [InlineData(3)]
+        [InlineData(6)]
+        [InlineData(9)]
+        [InlineData(12)]
+        public void FizzBuzz_WhenDiv3_ReturnFizz(int input)
         {
             //Arrange
             var expected = "FIZZ";
 
             //Act
-            var actual = sut.GetValue(3);
+            var actual = sut.GetValue(input);
 
             //Assert
             Assert.Equal(expected, actual);
         }
 
-        [Fact]
-        public void FizzBuzz_When5_ReturnBuzz()
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]        
+        public void FizzBuzz_WhenDiv5_ReturnBuzz(int input)
         {
             //Arrange            
             var expected = "BUZZ";
 
             //Act
-            var actual = sut.GetValue(5);
+            var actual = sut.GetValue(input);
 
             //Assert
             Assert.Equal(expected, actual);
         }
 
-        [Fact]
-        public void FizzBuzz_When0_ReturnFizzBuzz()
+        [Theory]
+        [InlineData(0)]
+        [InlineData(15)]
+        public void FizzBuzz_WhenDiv3and5_ReturnFizzBuzz(int input)
         {
             //Arrange            
             var expected = "FIZZBUZZ";
 
             //Act
-            var actual = sut.GetValue(0);
+            var actual = sut.GetValue(input);
 
             //Assert
             Assert.Equal(expected, actual);
